1e6cafaf0a3515e4421e5098684e70182abcc426
DanilFunDev <zhulinskidanil@gmail.com>
2022-10-25 18:14:39 +0300

    first commit

---
+++ JS/main.js
@@ -0,0 +1,69 @@
+const cells = 31
+
+// From 0.001 to 100
+const items = [
+  {name: 'iPhone', img: 'IMG/case/iPhone.png', chance: 10},
+  {name: 'Keyboard', img: 'IMG/case/keyboard.png', chance: 25},
+  {name: 'Headphones', img: 'IMG/case/headphones.png', chance: 40}
+]
+
+function getItem() {
+  let item;
+
+  while (!item) {
+    const chance = Math.floor(Math.random() * 100)
+    
+    items.forEach(elm => {
+      if (chance < elm.chance && !item) item = elm
+    })
+  }
+
+  return item
+}
+
+function generateItems() {
+  document.querySelector('.list').remove()
+  document.querySelector('.scope').innerHTML = `
+    <ul class="list"></ul>
+  `
+  
+  const list = document.querySelector('.list')
+
+  for (let i = 0; i < cells; i++) {
+    const item = getItem()
+    
+    const li = document.createElement('li')
+    li.setAttribute('data-item', JSON.stringify(item))
+    li.classList.add('list__item')
+    li.innerHTML = `
+      <img src="${item.img}" alt="" />
+    `
+
+    list.append(li)
+  }
+}
+
+generateItems()
+
+let isStarted = false
+
+function start() {
+  if (isStarted) return
+  else isStarted = true
+
+  generateItems()
+  const list = document.querySelector('.list')
+
+  setTimeout(() => {
+    list.style.left = '50%'
+    list.style.transform = 'translate3d(-50%, 0, 0)'
+  }, 0)
+
+  const item = list.querySelectorAll('li')[15]
+
+  list.addEventListener('transitionend', () => {
+    isStarted = false
+    item.classList.add('active')
+    const data = JSON.parse(item.getAttribute('data-item'))
+  })
+}
